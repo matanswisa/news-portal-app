d22e54989b28dbe88fce20eb213c487f9088d12d
matanswisa <matanswisa01@gmail.com>
2022-08-17 14:44:16 +0300

    config postgres db connection using docker

--- server/news_app/models.py
+++ server/news_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class NewsPosts(models.Model):
+
+    title = models.CharField(max_length=150)
+
+    content = models.TextField()
+
+    image_url = models.TextField()
+    
